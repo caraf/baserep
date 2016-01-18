52ed1cdc279f9450285b6f064c9e6f55884a970a
Cara Franco <xbbkbnv@eagleinvsys.com>
2016-01-18 14:58:02 -0500

    open weather app commit

---
+++ angular-weather-app/app/home/home.js
@@ -0,0 +1,15 @@
+viewsModule.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when("/", {
+    templateUrl : "./home/home.html",
+    controller : 'HomeCtrl'
+  });
+}]);
+
+viewsModule.controller('HomeCtrl', ['$scope', 'owmUSCities', 'owmHistory', function($scope, owmUSCities, owmHistory) {
+    owmHistory.push({name : "Home", isHome : true});
+    owmUSCities()
+    .then(function(citiesXhr) {
+      $scope.cities = citiesXhr;
+    });
+}]);
+
